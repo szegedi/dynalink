964c137ab8ff0690688a2e8cc69bc35ad80d5bc0
Attila Szegedi <szegedia@gmail.com>
2012-07-18 17:47:13 -0700

    Remove unconditional type converter shortcut; it's not needed. Also, use GuardedInvocation.compose() instead of guardWithTest().

--- src/main/java/org/dynalang/dynalink/support/TypeConverterFactory.java
+++ src/main/java/org/dynalang/dynalink/support/TypeConverterFactory.java
@@ -223,11 +223,7 @@ public class TypeConverterFactory {
             final GuardedInvocation next = factories[i].convertToType(sourceType, targetType);
             if(next != null) {
                 next.assertType(type);
-                if(next.getGuard() == null) {
-                    // If a converter is unconditional, choose it as the only converter
-                    return next.getInvocation();
-                }
-                last = MethodHandles.guardWithTest(next.getGuard(), next.getInvocation(), last);
+                last = next.compose(last);
             }
         }
         return last == identity ? IDENTITY_CONVERSION : last;
