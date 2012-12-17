3b7a27c655c4e524dc97d4fcff70eafdb97261bb
Attila Szegedi <szegedia@gmail.com>
2012-12-17 18:50:05 +0100

    Adding/fixing up license boilerplates.

--- src/main/java/org/dynalang/dynalink/support/Lookup.java
+++ src/main/java/org/dynalang/dynalink/support/Lookup.java
@@ -13,6 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
+
 package org.dynalang.dynalink.support;
 
 import java.lang.invoke.MethodHandle;
