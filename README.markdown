fdf6c8f372db7551a07723c763be8087666ca6c0
Attila Szegedi <szegedia@gmail.com>
2013-02-07 16:17:04 +0100

    Dual-licensing Dynalink under both Apache 2.0 and 3-clause BSD license

--- README.markdown
+++ README.markdown
@@ -18,6 +18,10 @@ If you just want the binaries, you can get the releases from the
 Frequent snapshots of the version under development are available in the [
 Maven snapshot repository](https://oss.sonatype.org/content/repositories/snapshots/org/dynalang/dynalink).
 
-Dynalink is Open Source software, licensed under the Apache 2.0 license.
+Dynalink is Open Source software, dual-licensed under both the Apache 2.0
+license and 3-clause BSD license, allowing you to choose either one when you
+use Dynalink. When you choose one license, it will be the only one that
+applies to you, the other does not. See the [Licensing FAQ](https://github.com/szegedi/dynalink/wiki/Licensing-FAQ)
+for advice on which one to choose.
 
 [![Build Status](https://secure.travis-ci.org/szegedi/dynalink.png)](http://travis-ci.org/szegedi/dynalink)
--- src/main/java/org/dynalang/dynalink/support/NamedDynCallSiteDescriptor.java
+++ src/main/java/org/dynalang/dynalink/support/NamedDynCallSiteDescriptor.java
@@ -1,18 +1,28 @@
 /*
-   Copyright 2009-2012 Attila Szegedi
+   Copyright 2009-2013 Attila Szegedi
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+   Licensed under either the Apache License, Version 2.0 (the "Apache
+   License") or the 3-clause BSD License (the "BSD License"), with licensee
+   being free to choose either of the two at their discretion.
 
-       http://www.apache.org/licenses/LICENSE-2.0
+   You may not use this file except in compliance with either the Apache
+   License or the BSD License.
+
+   A copy of the BSD License is available in the root directory of the
+   source distribution of the project under the file name
+   "LICENSE-BSD.txt".
+
+   A copy of the Apache License is available in the root directory of the
+   source distribution of the project under the file name
+   "LICENSE-Apache-2.0.txt". Alternatively, you may obtain a copy of the
+   Apache License at <http://www.apache.org/licenses/LICENSE-2.0>
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
+   See your chosen License for the specific language governing permissions
+   and limitations under that License.
+*/
 
 package org.dynalang.dynalink.support;
 
